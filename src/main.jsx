d79e4beb116f64dd6705132c08e094f70b76ad53
Khatrip009 <Khatrip.009@gmail.com>
2026-08-07 13:40:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'           // ✅ change to HashRouter
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
@@ -14,7 +14,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>                                       {/* ✅ use HashRouter */}
         <AuthProvider>
           <OrganizationProvider>
             <ThemeProvider>
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             </ThemeProvider>
           </OrganizationProvider>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 )
